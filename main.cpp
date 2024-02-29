4e89d57e33fdbd73bb242b31a191685325fc089f
Fabian <grossfa2@students.zhaw.ch>
2024-02-29 15:23:55 +0100

    Praktikum 2 Aufgabe 2 fini

--- main.cpp
+++ main.cpp
@@ -7,13 +7,14 @@
 #include "IRSensor.h"
 
 
+
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
 //de Luca wett kassiere
 
 int main()
 {
-    //Pins definieren
+    //Pins definieren für IR Sensoren
     DigitalOut led0(PD_4);
     DigitalOut led1(PD_3);
     DigitalOut led2(PD_6);
@@ -26,6 +27,13 @@ int main()
     DigitalOut bit1(PF_1);
     DigitalOut bit2(PF_2);
 
+    //Pins definieren für PWM
+    DigitalOut enableMotorDriver(PG_0);
+    DigitalIn motorDriverFault(PD_1);
+    DigitalIn motorDriverWarning(PD_0);
+    PwmOut pwmLeft(PF_9);
+    PwmOut pwmRight(PF_8);
+
     enable = 1; // schaltet die IR Distanzsensoren ein
 
     //Sensor Objekte erstellen
@@ -36,6 +44,13 @@ int main()
     IRSensor irSensor4(distance, bit0, bit1, bit2, 4);
     IRSensor irSensor5(distance, bit0, bit1, bit2, 5);
 
+    //Encoder Initialisieren
+    pwmLeft.period(0.00005); // Setzt die Periode auf 50 µs
+    pwmRight.period(0.00005);
+    pwmLeft = 0.5; // Setzt die Duty-Cycle auf 50%
+    pwmRight = 0.5;
+    enableMotorDriver = 1; // Schaltet den Leistungstreiber ein
+
     while (true) {
         led0 = irSensor0 < 0.2f;
         led1 = irSensor1 < 0.2f;
@@ -43,5 +58,8 @@ int main()
         led3 = irSensor3 < 0.2f;
         led4 = irSensor4 < 0.2f;
         led5 = irSensor5 < 0.2f;
+
+        pwmLeft = 0.7;
+        pwmRight = 0.7;
     }
 }
